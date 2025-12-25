9ed433a65ec95be8cbffd3aa681056f86201d4bb
Base21rizz <shoumikislamofficial@gmail.com>
2025-12-25 13:48:33 +0600

    Operator

---
+++ Operators.py
@@ -0,0 +1,17 @@
+age = 21
+height = 6.1
+complex = 3 + 4j
+print(age)
+print(height)
+print(complex)
+
+base = input("Enter base: ")
+height = input("Enter height: ")
+area = 0.5 * float(base) * float(height)
+print("Area of triangle is:", area)
+
+side_a = input("Enter side a: ")
+side_b = input("Enter side b: ")
+side_c = input("Enter side c: ")
+perimeter = float(side_a) + float(side_b) + float(side_c)
+print("Perimeter of triangle is:", perimeter)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+age = 21
+height = 6.1
+complex = 3 + 4j
+print(age)
+print(height)
+print(complex)
