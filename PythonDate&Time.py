d52618b9fdfe31bd6480dc650b262acbabb77716
Base21rizz <shoumikislamofficial@gmail.com>
2026-01-16 00:24:03 +0600

    Without Formatting Date Output Using strftime

--- PythonDate&Time.py
+++ PythonDate&Time.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 # Getting datetime Information
-
 now = datetime.now()
 day = now.day
 month = now.month
@@ -13,3 +12,23 @@ print(now)
 timestamp = now.timestamp()
 print(timestamp)
 print(f'{day}/{month}/{year}, {hour}:{minute}')
+
+# Without Formatting Date Output Using strftime
+""" new_year = datetime(2026, 1, 1)
+print(new_year)
+day = new_year.day
+month = new_year.month
+year = new_year.year
+hour = new_year.hour
+minute = new_year.minute
+second = new_year.second
+print(f'{day}/{month}/{year}, {hour}:{minute}') """
+
+# Formatting Date Output Using strftime
+now = datetime.now()
+t = now.strftime("%H:%M:%S")
+print("time:", t)
+time_one = now.strftime("%m/%d/%Y, %H,%M,%S")
+print("time one:", time_one)
+time_two = now.strftime("%d/%m/%Y, %H:%M:%S")
+print("time now:", time_two)
